8e94042fb05636ca45784ba6422662cbb3e80989
Víctor Alfonso Montero Javier <victor.alf-11@hotmail.com>
2020-01-09 02:44:41 -0400

    Agregar registro estudiantes - by ClaudioArturo30

--- IDS323-MiIndiceAcademico/MiIndiceAcademico_F1/Registro_Estudiantes.Designer.cs
+++ IDS323-MiIndiceAcademico/MiIndiceAcademico_F1/Registro_Estudiantes.Designer.cs
@@ -27,19 +27,222 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textname = new System.Windows.Forms.TextBox();
+            this.textapellido = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.cContraseña = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textname
+            // 
+            this.textname.Location = new System.Drawing.Point(261, 83);
+            this.textname.Name = "textname";
+            this.textname.Size = new System.Drawing.Size(323, 22);
+            this.textname.TabIndex = 3;
+            this.textname.TextChanged += new System.EventHandler(this.textname_TextChanged);
+            // 
+            // textapellido
+            // 
+            this.textapellido.Location = new System.Drawing.Point(261, 125);
+            this.textapellido.Name = "textapellido";
+            this.textapellido.Size = new System.Drawing.Size(323, 22);
+            this.textapellido.TabIndex = 4;
+            this.textapellido.TextChanged += new System.EventHandler(this.textapellido_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(480, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Email:";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(261, 203);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(323, 22);
+            this.textEmail.TabIndex = 14;
+            this.textEmail.TextChanged += new System.EventHandler(this.textEmail_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Confirmar Contraseña:";
+            // 
+            // contraseña
+            // 
+            this.contraseña.Location = new System.Drawing.Point(261, 241);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(323, 22);
+            this.contraseña.TabIndex = 17;
+            // 
+            // cContraseña
+            // 
+            this.cContraseña.Location = new System.Drawing.Point(261, 279);
+            this.cContraseña.Name = "cContraseña";
+            this.cContraseña.Size = new System.Drawing.Size(323, 22);
+            this.cContraseña.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(200, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 17);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(261, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 22);
+            this.textBox1.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Carrera: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(261, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(323, 22);
+            this.textBox2.TabIndex = 22;
             // 
             // Registro_Estudiantes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(822, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cContraseña);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textapellido);
+            this.Controls.Add(this.textname);
             this.Name = "Registro_Estudiantes";
             this.Text = "Registro_Estudiantes";
+            this.Load += new System.EventHandler(this.Registro_Estudiantes_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox textname;
+        private System.Windows.Forms.TextBox textapellido;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.TextBox cContraseña;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- IDS323-MiIndiceAcademico/MiIndiceAcademico_F1/Registro_Estudiantes.cs
+++ IDS323-MiIndiceAcademico/MiIndiceAcademico_F1/Registro_Estudiantes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MiIndiceAcademico_F1
 {
@@ -15,6 +16,55 @@ namespace MiIndiceAcademico_F1
         public Registro_Estudiantes()
         {
             InitializeComponent();
+        }
+
+        private void Registro_Estudiantes_Load(object sender, EventArgs e)
+        {
+
+        }    
+         
+        private void textname_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(contraseña.Text != cContraseña.Text)
+            {
+                MessageBox.Show("La contraseña no coincide con la confirmacion favor intentar de nuevo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else
+            {
+                StreamWriter guardar = null;
+                guardar = File.CreateText("Registro.txt");
+                string contenido = textBox1.Text + ",";
+                string contenido2 = textname.Text + ",";
+                string contenido3 = textBox2.Text + ",";
+                guardar.Write(contenido);
+                guardar.Write(contenido2);
+                guardar.Write(contenido3);
+                guardar.Flush();
+                guardar.Close();
+                MessageBox.Show("El registro se completo exitosamente!", "Message", MessageBoxButtons.OK);
+                this.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+
+        private void textapellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textEmail_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
