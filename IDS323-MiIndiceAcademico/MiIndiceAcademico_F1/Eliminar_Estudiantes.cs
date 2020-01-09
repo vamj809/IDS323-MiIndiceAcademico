3918411961f69fdd362c7e92ce8b3ace6fc9b20e
ClaudioArturo30 <58832771+ClaudioArturo30@users.noreply.github.com>
2020-01-09 04:01:08 -0400

    Add files via upload

--- IDS323-MiIndiceAcademico/MiIndiceAcademico_F1/Eliminar_Estudiantes.cs
+++ IDS323-MiIndiceAcademico/MiIndiceAcademico_F1/Eliminar_Estudiantes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MiIndiceAcademico_F1
 {
@@ -15,6 +16,70 @@ namespace MiIndiceAcademico_F1
         public Eliminar_Estudiantes()
         {
             InitializeComponent();
+        }
+
+        private void eliminar_Click(object sender, EventArgs e)
+        {
+            StreamReader Lector;
+            bool encontrar;
+            encontrar = false;
+            String[] longitud = new String[99];
+            String Cadenas;
+            StreamWriter escribir;
+            escribir = File.CreateText("copia.txt");
+            try
+            {
+                Lector = File.OpenText("Registro.txt");
+
+                string id = textBox1.Text;
+                Cadenas = Lector.ReadLine();
+                while (Cadenas != null)
+                {
+
+                    longitud = Cadenas.Split(',');
+                    if (longitud[0].Trim().Equals(id))
+                    {
+                        Console.WriteLine("Nombre: " + longitud[0].Trim());
+                        encontrar = true;
+
+                    }
+                    else
+                    {
+                        escribir.WriteLine(Cadenas);
+
+                    }
+                    Cadenas = Lector.ReadLine();
+
+                }
+                if (encontrar == false)
+                {
+                    MessageBox.Show("La ID no es correcta o no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+                else
+                {
+                    MessageBox.Show("La Eliminacion se completo exitosamente!", "Message", MessageBoxButtons.OK);
+
+                }
+                Lector.Close();
+                escribir.Close();
+
+                File.Delete("Registro.txt");
+                File.Move("copia.txt", "Registro.txt");
+            }
+            catch (FileNotFoundException fe)
+            {
+                Console.WriteLine("Error!!" + fe.Message);
+
+            }
+
+            this.Close();
+           
+        }
+
+        private void cancelar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
