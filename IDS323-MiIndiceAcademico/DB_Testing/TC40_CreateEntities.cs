c45c9fb7cb5811d4bf21c23c9aeea6718901bcf8
Víctor A. Montero <vamj809@users.noreply.github.com>
2021-01-20 21:41:48 -0400

    Added main entities for db/dbi testing

--- IDS323-MiIndiceAcademico/DB_Testing/TC40_CreateEntities.cs
+++ IDS323-MiIndiceAcademico/DB_Testing/TC40_CreateEntities.cs
@@ -15,17 +15,17 @@ namespace DB_Testing
 
         /* ------------------- Estudiante ------------------- */
         public Estudiante CrearEstudiante(string nombre, string carrera, string clave) {
-            string new_id;
+            int new_id;
 
             if (CC.Estudiantes.Count() < 1) {
-                new_id = 1201001.ToString();
+                new_id = 1201001;
             }
             else {
-                new_id = (CC.Estudiantes.Max(x => x.ID_Estudiante) + 1).ToString();
+                new_id = (CC.Estudiantes.Max(x => x.ID_Estudiante) + 1);
             }
 
             return new Estudiante() {
-                ID_Estudiante = int.Parse(new_id),
+                ID_Estudiante = new_id,
                 Nombre_Estudiante = nombre,
                 Carrera_Estudiante = carrera,
                 Clave_Estudiante = clave
@@ -33,7 +33,7 @@ namespace DB_Testing
         }
 
         [Test]
-        public void TC40_UC01_CrearEstudiante() {
+        public void TC40_CrearEstudiante() {
             Estudiante EntradaEstudiante = CrearEstudiante("Mr. Prueba TC40", "SQA", "1aF1#@1957");
 
             CC.Estudiantes.Add(EntradaEstudiante);
@@ -58,24 +58,24 @@ namespace DB_Testing
         /* ------------------- Profesor ------------------- */
 
         public Profesor CrearProfesor(string nombre, string clave) {
-            string new_id;
+            int new_id;
 
             if (CC.Profesores.Count() < 1) {
-                new_id = 220001.ToString();
+                new_id = 220001;
             }
             else {
-                new_id = (CC.Profesores.Max(x => x.ID_Profesor) + 1).ToString();
+                new_id = (CC.Profesores.Max(x => x.ID_Profesor) + 1);
             }
 
             return new Profesor() {
-                ID_Profesor = int.Parse(new_id),
+                ID_Profesor = new_id,
                 Nombre_Profesor = nombre,
                 Clave_Profesor = clave
             };
         }
 
         [Test]
-        public void TC40_UC02_CrearProfesor() {
+        public void TC41_CrearProfesor() {
             Profesor EntradaProfesor = CrearProfesor("El Profe TC40_UC02", "Acz9dm@2324.net");
 
             CC.Profesores.Add(EntradaProfesor);
@@ -91,6 +91,49 @@ namespace DB_Testing
                     EntradaProfesor.ID_Profesor     != SalidaProfesor.ID_Profesor ||
                     EntradaProfesor.Nombre_Profesor != SalidaProfesor.Nombre_Profesor ||
                     EntradaProfesor.Clave_Profesor  != SalidaProfesor.Clave_Profesor) {
+                Assert.Fail();
+            }
+            Assert.Pass();
+        }
+
+        /* ------------------- Administrador ------------------- */
+
+        public Administrador CrearAdministrador(string nombre, string clave)
+        {
+            int new_id;
+
+            if (CC.Administradores.Count() < 1) {
+                new_id = 320001;
+            }
+            else {
+                new_id = (CC.Administradores.Max(x => x.Usuario) + 1);
+            }
+
+            return new Administrador() {
+                Usuario = new_id,
+                Nombre = nombre,
+                Clave = clave
+            };
+        }
+
+        [Test]
+        public void TC42_CrearAdministrador()
+        {
+            Administrador EntradaAdministrador = CrearAdministrador("Sr. Gerente", "my4dm1np455w0rd");
+
+            CC.Administradores.Add(EntradaAdministrador);
+            CC.GuardarAdministradores();
+            // ----------------------------------------- Ahora a buscar en los datos.
+            var NuevaColeccion = new ColeccionCompleta();
+            NuevaColeccion.RecargarAdministradores();
+
+            Administrador SalidaAdministrador = NuevaColeccion.Administradores.Where(x => x.Usuario == EntradaAdministrador.Usuario)?.First();
+
+            //Primero, valida si se guardó; luego compara la consistencia en los datos.
+            if (SalidaAdministrador == null ||
+                    EntradaAdministrador.Usuario != SalidaAdministrador.Usuario ||
+                    EntradaAdministrador.Nombre != SalidaAdministrador.Nombre ||
+                    EntradaAdministrador.Clave != SalidaAdministrador.Clave) {
                 Assert.Fail();
             }
             Assert.Pass();
